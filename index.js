fa96901542b972fd45f789a980a584171627ad9e
santino2221 <166014288+santino2221@users.noreply.github.com>
2024-12-12 09:55:54 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,21 @@
+let n1 = Number (prompt('Ingresa un valor') )
+let n2 =  Number (prompt('Ingresa un valor') ) 
+let nombre = prompt ("ingresa un nombre")
+console.log (n1+n2) 
+console.log (n1-n2) 
+console.log (n1*n2)
+console.log  (n1/n2)
+alert("hola" + nombre)
+
+if ("si estas aprobado>estas desaprobado") {
+    alert("estas aprobado")
+    
+} else {
+  alert("estas desaprobado")  
+}
+
+if ("sacar la basura lunes miercoles jueves sabado residuos secos, martes viernes y domingo humedos") {
+    console.log("escribir un dia de la semana")
+} else {
+    
+}
